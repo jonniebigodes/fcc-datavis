226925a1715f8e5fc2aeb7acb0ad72ba08030362
jonniebigodes <jonniebigodes@hotmail.com>
2018-07-21 22:56:21 +0100

    regionized and prettified. Corrected some props entries needed

--- src/components/Challenges/GlobeChartView/MeteorToolTip.js
+++ src/components/Challenges/GlobeChartView/MeteorToolTip.js
@@ -1,33 +1,43 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import styles from './globe-style.module.css';
-const showMeteorInfo=()=>{
-    return(
-        <p><span className={styles.tooltipText}>Mouse over map to show information</span></p>
-    )
-};
-const showMeteorData=value=>{
-    return(
-        <p><span className={styles.tooltipText}>In {value.dateofhit} a meteor with mass of {value.meteormass} and class {value.meteorclass} fell on latitude {value.latitude} longitude {value.longitude}</span></p>
-    )
-};
+import PropTypes from 'prop-types'
+import React from 'react'
+import styles from './globe-style.module.css'
 
-const MeteorToolTip=({data})=>{
+const showMeteorInfo = () => {
+  return (
+    <p>
+      <span className={styles.tooltipText}>
+        Mouse over map to show information
+      </span>
+    </p>
+  )
+}
+const showMeteorData = value => {
+  return (
+    <p>
+      <span className={styles.tooltipText}>
+        In {value.dateofhit} a meteor with mass of {value.meteormass} and class{' '}
+        {value.meteorclass} fell on latitude {value.latitude} longitude{' '}
+        {value.longitude}
+      </span>
+    </p>
+  )
+}
 
-    return(
-        <div className={styles.containerToolTip}>
-            {data?showMeteorData(data):showMeteorInfo()}
-        </div>
-    );
-};
-MeteorToolTip.propTypes={
-    data:PropTypes.shape({
-        name:PropTypes.string,
-        meteorclass:PropTypes.string,
-        dateofhit:PropTypes.string,
-        meteormass:PropTypes.number,
-        latitude:PropTypes.string,
-        longitude:PropTypes.string
-    })
-};
-export default MeteorToolTip;
+const MeteorToolTip = ({ data }) => {
+  return (
+    <div className={styles.containerToolTip}>
+      {data ? showMeteorData(data) : showMeteorInfo()}
+    </div>
+  )
+}
+MeteorToolTip.propTypes = {
+  data: PropTypes.shape({
+    name: PropTypes.string,
+    meteorclass: PropTypes.string,
+    dateofhit: PropTypes.string,
+    meteormass: PropTypes.number,
+    latitude: PropTypes.string,
+    longitude: PropTypes.string,
+  }),
+}
+export default MeteorToolTip
--- src/components/Challenges/GlobeChartView/WorldMap.js
+++ src/components/Challenges/GlobeChartView/WorldMap.js
@@ -1,63 +1,76 @@
-import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
-import MeteorPoint from './MeteorPoint';
-class WorldMap extends PureComponent{
-    onPointHover=value=>{
-        const {pointEnter}= this.props;
-        pointEnter(value);
-    }
-    onPointLeave=()=>{
-        const {pointExit}= this.props;
-        pointExit();
-    }
-    render(){
-        const {world,meteorfall}= this.props;
-       
-        const parsedWorld=(
-            world.map((d,i)=><path key={`path:${i}`} d={d.dpath} fill={d.fillInfo} stroke="#FFFFFF" strokeWidth={0.5}/>,)
-        )
-        
-        const meteorsData=(
-            meteorfall.map((m,x)=>
-                <MeteorPoint key={`mpoint_${x}`}
-                    data={{
-                        meteorname:m.name,
-                        meteorclass:m.meteorClass,
-                        datefell:m.date,
-                        mass:m.mass,
-                        rectangleLat:m.latitude,
-                        rectangleLong:m.longitude,
-                        circleId:x,
-                        circleArea:m.radius,
-                        circleFill:m.fillOp
-                    }}
-                    meteorPointEnter={this.onPointHover}
-                    meteorPointExit={this.onPointLeave}/>,
-            )
-        )
-        return(
-            <g>
-                {parsedWorld}
-                {meteorsData}
-            </g>
-        )
-    }
-    
+import PropTypes from 'prop-types'
+import uuid from 'uuid'
+import React, { PureComponent } from 'react'
+import MeteorPoint from './MeteorPoint'
+
+class WorldMap extends PureComponent {
+  onPointHover = value => {
+    const { pointEnter } = this.props
+    pointEnter(value)
+  }
+  onPointLeave = () => {
+    const { pointExit } = this.props
+    pointExit()
+  }
+  render() {
+    const { world, meteorfall } = this.props
+
+    const parsedWorld = world.map(d => (
+      <path
+        key={`path:${uuid.v4()}`}
+        d={d.dpath}
+        fill={d.fillInfo}
+        stroke="#FFFFFF"
+        strokeWidth={0.5}
+      />
+    ))
+
+    const meteorsData = meteorfall.map((m, x) => (
+      <MeteorPoint
+        key={`mpoint_${m.name}_class_${m.meteorclass}`}
+        data={{
+          meteorname: m.name,
+          meteorclass: m.meteorClass,
+          datefell: m.date,
+          mass: m.mass,
+          rectangleLat: m.latitude,
+          rectangleLong: m.longitude,
+          circleId: x,
+          circleArea: m.radius,
+          circleFill: m.fillOp,
+        }}
+        meteorPointEnter={this.onPointHover}
+        meteorPointExit={this.onPointLeave}
+      />
+    ))
+    return (
+      <g>
+        {parsedWorld}
+        {meteorsData}
+      </g>
+    )
+  }
 }
-WorldMap.propTypes={
-    world:PropTypes.arrayOf(PropTypes.shape({
-        dpath:PropTypes.string,
-        fillInfo:PropTypes.string
-    })),
-    meteorfall:PropTypes.arrayOf(PropTypes.shape({
-        date:PropTypes.string,
-        fillOp:PropTypes.number,
-        latitude:PropTypes.number,
-        longitude:PropTypes.number,
-        mass:PropTypes.number,
-        name:PropTypes.string,
-        radius:PropTypes.number,
-        meteorClass:PropTypes.string
-    }))
+WorldMap.propTypes = {
+  world: PropTypes.arrayOf(
+    PropTypes.shape({
+      dpath: PropTypes.string,
+      fillInfo: PropTypes.string,
+    })
+  ),
+  pointEnter: PropTypes.func,
+  pointExit: PropTypes.func,
+  meteorfall: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.string,
+      fillOp: PropTypes.number,
+      latitude: PropTypes.number,
+      longitude: PropTypes.number,
+      mass: PropTypes.number,
+      name: PropTypes.string,
+      radius: PropTypes.number,
+      meteorClass: PropTypes.string,
+    })
+  ),
 }
-export default WorldMap;
+export default WorldMap
