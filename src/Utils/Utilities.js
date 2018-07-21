6382e37e0eac1a90061f377bbc9a38486e660522
jonniebigodes <jonniebigodes@hotmail.com>
2018-07-21 23:21:42 +0100

    final changes before build

--- src/Utils/Utilities.js
+++ src/Utils/Utilities.js
@@ -1,16 +1,17 @@
-import {dataVisConstant} from './Constants';
-class Utilities{
-    static setStorageData(token,value){
-        localStorage.setItem(token,JSON.stringify(value));
-    }
-    static getStorageData(value){
-        return localStorage.getItem(value);
-    }
-    static clearStorage(){
-        localStorage.clear();
-    }
-    static loadCountryInfo(value){
-        return dataVisConstant.countryInfo[value];
-    }
+import { dataVisConstant } from './Constants'
+
+class Utilities {
+  static setStorageData(token, value) {
+    localStorage.setItem(token, JSON.stringify(value))
+  }
+  static getStorageData(value) {
+    return localStorage.getItem(value)
+  }
+  static clearStorage() {
+    localStorage.clear()
+  }
+  static loadCountryInfo(value) {
+    return dataVisConstant.countryInfo[value]
+  }
 }
-export default Utilities;
+export default Utilities
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,39 +3,38 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import Footer from '../components/Footer/footer';
+import Footer from '../components/Footer/footer'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
-  
   <div>
     <Helmet
       title="freeCodeCamp Data Visualization Projects"
       meta={[
         { name: 'description', content: 'freeCodeCamp Data Visualization' },
         { name: 'keywords', content: 'react, d3, data visualization,gatsby' },
-        {name:'author',content:'jonniebigodes'}
+        { name: 'author', content: 'jonniebigodes' },
       ]}
       link={[
-        {rel:"stylesheet",href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"}
+        {
+          rel: 'stylesheet',
+          href:
+            'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css',
+        },
       ]}
-      // script={[
-      //   {rel: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css", rel: "stylesheet"}
-      // ]}
     />
-    <Header/>
+    <Header />
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
-      }}
-    >
+      }}>
       {children()}
     </div>
     <div>
-      <Footer/>
+      <Footer />
     </div>
   </div>
 )
